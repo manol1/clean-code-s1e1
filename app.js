a364a3fafea40c6d514a4665830c2f2461090287
manol1 <marsen1491@gmail.com>
2021-11-29 14:09:14 +0300

    refactor: adjust CSS-formatting according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput = document.getElementById("new-task");//Add a new task.
-var addButton = document.getElementsByTagName("button")[0];//first button
+var addButton = document.querySelector(".add-button");//first button
 var incompleteTaskHolder = document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
 
@@ -33,20 +33,23 @@ var createNewTaskElement = function (taskString) {
     var deleteButtonImg = document.createElement("img");//delete button image
 
     label.innerText = taskString;
-    label.className = 'task';
+    label.className = 'task label';
 
     //Each elements, needs appending
     checkBox.type = "checkbox";
+    checkBox.className = "input";
     editInput.type = "text";
-    editInput.className = "task";
+    editInput.className = "task input";
 
     editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className = "edit";
+    editButton.className = "edit button";
 
-    deleteButton.className = "delete";
+    deleteButton.className = "delete button";
+    deleteButtonImg.className = "delete-img";
     deleteButtonImg.src = './remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
+    listItem.className = "list-item";
 
     //and appending.
     listItem.appendChild(checkBox);
@@ -82,8 +85,8 @@ var editTask = function () {
 
     var listItem = this.parentNode;
 
-    var editInput = listItem.querySelector('input[type="text"]');
-    var label = listItem.querySelector("label");
+    var editInput = listItem.querySelector('.input[type="text"]');
+    var label = listItem.querySelector(".label");
     var editBtn = listItem.querySelector(".edit");
     var containsClass = listItem.classList.contains("edit-mode");
     //If class of the parent is .edit-mode
@@ -155,9 +158,9 @@ addButton.addEventListener("click", ajaxRequest);
 var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
     console.log("bind list item events");
     //select ListItems children
-    var checkBox = taskListItem.querySelector("input[type=checkbox]");
-    var editButton = taskListItem.querySelector("button.edit");
-    var deleteButton = taskListItem.querySelector("button.delete");
+    var checkBox = taskListItem.querySelector(".input[type=checkbox]");
+    var editButton = taskListItem.querySelector(".button.edit");
+    var deleteButton = taskListItem.querySelector(".button.delete");
 
 
     //Bind editTask to edit button.
